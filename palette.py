44f32eda3bddf083092360013d60b6d3dc683a4d
Tanveer Hussain <73167258+tanveerntu@users.noreply.github.com>
2022-01-22 21:40:41 +0500

    Update palette.py

--- palette.py
+++ palette.py
@@ -3,15 +3,17 @@ from skimage import io, color
 from skimage.transform import rescale, resize, downscale_local_mean
 import streamlit as st
 import pandas as pd
+import numpy as np
+
 from sklearn.cluster import KMeans
 from matplotlib.colors import to_hex
+from io import BytesIO
 
 st.set_page_config(
     page_title = 'Color Palette Generator',
     page_icon = '✅',
     layout = 'wide'
 )
-
 # ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
             <style>
@@ -22,43 +24,58 @@ hide_st_style = """
             """
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
-
-
 st.title("Color Palette Generator")
 st.header("Load any Image to Generate its Color Palette")
-st.write("The color palette will appear below the loaded images after a few seconds, depending on your internet speed.")
-number = st.slider('Max. Number of shades in the Palette', 0, 20, 5)
+st.write("Palette Generation may take a few seconds after loading the image")
 
+number = st.slider('Max. Number of colors in the Palette', 0, 20, 5)
 uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
 if uploaded_file is not None:
     image = io.imread(uploaded_file)
     st.image(image)
+    with st.spinner("Color Palette is being generated...."):
+        img = resize(image, (200, 200))
+        #st.image(img)
+        data = pd.DataFrame(img.reshape(-1, 3),
+                        columns=['R', 'G', 'B'])
 
-    img = resize(image, (200, 200))
-    #st.image(img)
-    data = pd.DataFrame(img.reshape(-1, 3),
-                    columns=['R', 'G', 'B'])
+        kmeans = KMeans(n_clusters=number,
+                    random_state=0)
+        # Fit and assign clusters
+        data['Cluster'] = kmeans.fit_predict(data)
+        palette = kmeans.cluster_centers_
 
-    kmeans = KMeans(n_clusters=number,
-                random_state=0)
-    # Fit and assign clusters
-    data['Cluster'] = kmeans.fit_predict(data)
-    palette = kmeans.cluster_centers_
+        # Convert data to format accepted by imshow
+        palette_list = list()
+        for color in palette:
+            palette_list.append([[tuple(color)]])
 
-    # Convert data to format accepted by imshow
-    palette_list = list()
-    for color in palette:
-        palette_list.append([[tuple(color)]])
+        # Show color palette
+        for color in palette_list:
+            #print(to_hex(color[0][0]))
 
-    # Show color palette
-    for color in palette_list:
-        fig = plt.figure(figsize=(6, 2))
-        st.header(to_hex(color[0][0]))
-        def display_table(fig, figsize=(5,5)):
-            "example of displaying a table in streamlit"
-            fig, ax = plt.subplots(figsize=figsize)
-            plt.axis('off')
-            plt.imshow(color, aspect='auto')
-            st.pyplot(fig)
+            fig = plt.figure(figsize=(6, 2))
+            #fig1, ax = plt.subplots(figsize=(6, 2))
+            #plt.axis('off')
+            #plt.imshow(color, aspect='auto')
+            #plt.show()
+            #st.image(fig1)
+            st.header(to_hex(color[0][0]))
+            #st.pyplot(fig)
+            #buf = BytesIO()
+            #fig.savefig(buf, format="png")
+            #st.image(buf)
 
-        display_table(fig, figsize=(10,5))
+            def display_table(fig, figsize=(5,5)):
+                "example of displaying a table in streamlit"
+                fig, ax = plt.subplots(figsize=figsize)
+                plt.axis('off')
+                plt.imshow(color, aspect='auto')
+                st.pyplot(fig)
+
+
+            display_table(fig, figsize=(10,3))
+            
+
+
+        
