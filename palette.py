c98e2d9abe191a5254c997f246fefee3f54d1921
Tanveer Hussain <73167258+tanveerntu@users.noreply.github.com>
2022-01-25 23:04:28 +0500

    Update palette.py

--- palette.py
+++ palette.py
@@ -35,36 +35,75 @@ if uploaded_file is not None:
     st.image(image)
     with st.spinner("Color Palette is being generated...."):
         img = resize(image, (200, 200))
-        #st.image(img)
+        #st.write(img.shape)
+        #img2 = img.reshape(-1, 3) # to reduce img in two dimensions
+        #st.write(img2.shape)
         data = pd.DataFrame(img.reshape(-1, 3),
-                        columns=['R', 'G', 'B'])
-
+                        columns=['R', 'G', 'B']) #creating R, G B columns of dataframe named data
         kmeans = KMeans(n_clusters=number,
                     random_state=0)
         # Fit and assign clusters
-        data['Cluster'] = kmeans.fit_predict(data)
-        palette = kmeans.cluster_centers_
+        data['Cluster'] = kmeans.fit_predict(data) # adding 'Cluster' column in dataframe 'data' 
+        palette = kmeans.cluster_centers_ # getting data table reduced to ceteroid clusters
+
+
+
+        data1 = (data
+            .groupby(['Cluster'])
+            .agg({
+            'Cluster': ['count'], 
+            'R': ['mean'],
+            'G': ['mean'],
+            'B': ['mean'],
+            }))
+        #data1 
+
+
+        def f(x):
+            d = {}
+            d['Cluster_count'] = x['Cluster'].count()
+            d['R_mean'] = x['R'].mean()
+            d['G_mean'] = x['G'].mean()
+            d['B_mean'] = x['B'].mean()
+
+            return pd.Series(d, index=['Cluster_count', 'R_mean', 'G_mean', 'B_mean'])
+
+        data2 = data.groupby('Cluster').apply(f)
+        data2 = data2.sort_values(["Cluster_count"], ascending=False)
+        #data2
+        data2["R_mean"] = 255 * data2["R_mean"]
+        data2["G_mean"] = 255 * data2["G_mean"]
+        data2["B_mean"] = 255 * data2["B_mean"]
+        data2 = data2.astype({"R_mean": int})
+        data2 = data2.astype({"G_mean": int})
+        data2 = data2.astype({"B_mean": int})
+
+
+        #data2
+        z = zip(data2['R_mean'], data2['B_mean'], data2['G_mean'])
+        data2["Hex"] = [f'#{R_mean:02X}{B_mean:02X}{G_mean:02X}' for R_mean, G_mean, B_mean in z]
+
+        #data2["Hex"] = data2.apply(f'#{R_mean:02X}{G_mean:02X}{B_mean:02X}' for R_mean, G_mean, B_mean in z)
+        #data2
+        fig4 = plt.figure(figsize=(3, 3))
+
+        plt.pie(data2["Cluster_count"], labels = data2["Hex"], colors=data2["Hex"], autopct='%1.1f%%')
+        plt.show()
+        st.pyplot(fig4)
+
+        #st.write(palette) #write data table generated after taking kmeans of clusters
 
         # Convert data to format accepted by imshow
         palette_list = list()
         for color in palette:
             palette_list.append([[tuple(color)]])
 
+        #st.write(color)
         # Show color palette
         for color in palette_list:
-            #print(to_hex(color[0][0]))
-
             fig = plt.figure(figsize=(6, 2))
-            #fig1, ax = plt.subplots(figsize=(6, 2))
-            #plt.axis('off')
-            #plt.imshow(color, aspect='auto')
-            #plt.show()
-            #st.image(fig1)
-            st.header(to_hex(color[0][0]))
-            #st.pyplot(fig)
-            #buf = BytesIO()
-            #fig.savefig(buf, format="png")
-            #st.image(buf)
+            st.header(to_hex(color[0][0])) # to print color hex numbers
+
 
             def display_table(fig, figsize=(5,5)):
                 "example of displaying a table in streamlit"
@@ -73,9 +112,27 @@ if uploaded_file is not None:
                 plt.imshow(color, aspect='auto')
                 st.pyplot(fig)
 
-
             display_table(fig, figsize=(10,3))
-            
 
 
-        
+
+            ###########
+            #Making elbow plot to see optimum clusters
+
+        # elbow method to identify optimal value of K
+        with st.spinner("Generating Elbow Plot to identify optimum number of palettes/clusters...."):
+            sse = [] #sume of squares
+            for k in range(1, 20):
+                km = KMeans(n_clusters=k)
+                km.fit(data)
+                sse.append(km.inertia_)
+
+            # plot to identify the elbow
+            fig2 = plt.figure(figsize=(6, 3))
+            plt.plot(range(1, 20), sse, '-o', c = 'maroon')
+            plt.xlabel('Count of Clusters')
+            plt.ylabel('SSE')
+            st.subheader("Elbow Plot to identify optimum number of palettes/clusters")
+            st.pyplot(fig2)
+
+ 
